fe48df26593dea7a86e444e00712ab633123f1dc
Nikola Stanković <stankovic.s.nikola@gmail.com>
2026-06-21 13:45:21 +0200

    Update ClientEffects.js

--- app/components/ClientEffects.js
+++ app/components/ClientEffects.js
@@ -118,6 +118,7 @@ export default function ClientEffects() {
               e.preventDefault();
               const offsetTop = target.offsetTop - 70;
               window.scrollTo({ top: offsetTop, behavior: 'smooth' });
+              history.pushState(null, null, href);
             }
           }
           return;
@@ -127,6 +128,7 @@ export default function ClientEffects() {
           e.preventDefault();
           const offsetTop = target.offsetTop - 70;
           window.scrollTo({ top: offsetTop, behavior: 'smooth' });
+          history.pushState(null, null, href);
         }
       };
       anchor.addEventListener('click', handler);
